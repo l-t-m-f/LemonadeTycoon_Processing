e880516ade7fd295aae1a1e71e031d2668ab7d6a
L_T_M_F <ltmf1990@protonmail.com>
2022-11-30 15:00:07 -0500

    Initial commit

---
+++ LemonadeTycoon.java
@@ -0,0 +1,27 @@
+import processing.core.PApplet;
+
+public class LemonadeTycoon extends PApplet {
+
+  public LemonadeTycoon() {
+  }
+
+  public void settings() {
+    size(1280, 720);
+  }
+
+  @Override
+  public void draw() {
+  }
+
+  @Override
+  public void mouseReleased() {
+
+  }
+
+  // Driver code
+  public static void main(String[] args)
+  {
+    PApplet.main( new String[] { "--present", "LemonadeTycoon" } );
+  }
+
+}
